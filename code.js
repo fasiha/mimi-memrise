2671e4a1296011317af09b0760117e0ff0532fe2
Ahmed Fasih <wuzzyview@gmail.com>
2014-10-28 16:05:16 -0400

    Need to automate tree-matching

--- code.js
+++ code.js
@@ -1,29 +1,9 @@
-var m =
-    "一二三四五六七八九十口日月田目古明晶品呂早世自白中昇上下朝員見元頑万句的首\
-乱直具真工左有切昭別町可子女好母小少大多夕汐外名石光太臭奇川水原願活消況土\
-時火点魚埋同洞向字守完安寄貯木森柏朴村相机本未末味若苦葉暮犬状黙然猫牛特告\
-先洗介界茶合塔王玉宝現全理主注柱金道導迫逃辺車連前格客夏条落冗運夢高塾京週\
-士売学覚書敗故言計話詩語読調談式試域成城止歩企歴正定走超越是題建遠初布市姉\
-製転雨雲冬天立泣章鐘商適敵叱頃北背比皆毎海乾腹複歌次姿音識境亡荒望方放激説\
-増贈東地虹風起記電家豪場羊美詳達差着誰集進雑準確午許習曜濯困国因回店庭磨心\
-忘認誌思恩応意想息恐惑感怖慌愉憶必手摩我議打捨指持拾描接掛研才財材存在及丈\
-史奴怒友抜投設支反坂返乳浮将受授愛払広鉱弁始窓去法会至室致互育流出山岩密崎\
-入込分貧公谷浴容欲常婆破残死瞬耳取最恥職聖敢聴懐慢買置環夫規替失蔵覧力男努\
-励加協行律得待彼役徴街程和私秘利米迷奥数類様求球笑答人住位仲体件仕他伝休仮\
-信例健側使便優傷付任代袋貸化花何荷俺内座挫卒傘以似宮年夜施遊旅物屋屈居局遅\
-沢訳戸房戻涙示礼社視由油届笛押申伸神捜果所近折断質昨作急寝婦掃当争事康君両\
-満画曲料図用備昔借散席度渡半伴判巻勝片不杯族知帰引強第与写身謝老考教者暑諸\
-追師父効校足路踏過際随陽院降隣隠空突究窟探深丘糸締練緒続絵絡結終紀紹経約絹\
-繰緑後幾機玄係懸服命冷鈴湧通危腕留印興酒配頭喜血盛爵退限眼良朗娘食飯飲館平\
-呼評希胸離殺純璧避新薪親幸報刻述寒譲嬢素麦青情晴静績表割生星性春実勤難華乗\
-今含念予預野嫌西価要南問間簡開閉聞非悲輩決快偉違干余除途束頼速整剣険重動働\
-種病痴症疲痛医迎発形影杉顔参修珍文対済楽率英映赤変跡恋横色絶甘期基貴無粗助\
-並普業僕共供異選囲悪触解再講構論編冊氏紙婚低民眠部郷響派脈段司伺詞飼航盤船\
-暇来気飛妻面声誤邪番長張展単戦弾悩厳島援偶逆揺晩勉象馬験駅騒驚能態振送関魔\
-雰也噂吠叶噌嬉牢靖醤雫澤";
-var PRIMITIVES = "艹亠聿戈𢦏巛巜⺀䒑儿" + "昜翟𠘨兑";
+var m = mimi790;
+m = mimi1503;
 
-KANJI += '澤';
+var PRIMITIVES = "艹亠聿戈𢦏巛巜⺀䒑儿" + "昜翟𠘨兑爫廾𠫓豕𦍌习殳禾氺";
+
+KANJI += '澤籠';
 KANJI = KANJI + PRIMITIVES;
 
 var kanji2number = {};
@@ -35,15 +15,15 @@ var db = {};
 
 var mimidb;
 
-var DONTINCLUDE = "洛或妾舜曼竹奄坐勿叩屯巽昏";
+var DONTINCLUDE = "洛或妾舜曼竹奄坐勿叩屯巽昏晃此";
 var MIMIPRIMITIVES =
     "升貝勺千舌刀召丁肖兄寺占里林右莫苗各軍享丸周吾炎制\
 衣云童匕乞曽羽廷麻忍志義舎旨圭台穴波旦呈列則吏更憂斤矢弔射孝交祭帝吉系県令\
-勇宛官凶辛亥責害兼門丙景彦斉黄般呉免敬辰鬼尊";
+勇宛官凶辛亥責害兼門丙景彦斉黄般呉免敬辰鬼尊刃酋" + "昌旧胡亘朋壮喬昆竜串荒戒乃";
 MIMIPRIMITIVES += DONTINCLUDE;
 // MIMIPRIMITIVES = "";
 
-var MIMIKANJI = MIMIPRIMITIVES + m;
+m = m + MIMIPRIMITIVES;
 
 d3.xhr('eids.txt', 'text/plain', function(err, req) {
     if (err) {
@@ -57,12 +37,18 @@ d3.xhr('eids.txt', 'text/plain', function(err, req) {
         db[match[1]] = {all : match[2], indent : match[3]};
     });
     mimidb = _.map(m.split(""), function(s) { return db[s]; });
+    // Sort in Heisig order (RTK1 vs 3 split :(
+    m = _.sortBy(m.split(""), function(s) { return kanji2number[s]; }).join("");
+
 
     // mimi kanji that could be simplified by pruning non-mimi
-    var mPrim = PRIMITIVES + MIMIKANJI;
+    var mPrim = PRIMITIVES + m;
     var noMatchRTK13VsMimi = compareStringsToPrune(m, mPrim);
     var carets2brackets =
         function(s) { return s.replace(/</g, "[").replace(/>/g, "]"); };
+    var indent2oneline = function(s) {
+        return s.replace(/\n +/g, '');
+    };
     if (noMatchRTK13VsMimi.length > 0) {
         d3.select("#cont")
             .selectAll("div")
@@ -81,8 +67,8 @@ d3.xhr('eids.txt', 'text/plain', function(err, req) {
             .enter()
             .append("div")
             .html(function(d, i) {
-            return "#" + kanji2number[d] + ":<br>" +
-                   carets2brackets(prune(db[d].indent, mPrim)) + "<hr>";
+            return "#" + kanji2number[d] + ": " +
+                   indent2oneline(carets2brackets(prune(db[d].indent, mPrim)));
         }).style("white-space", "pre");
     }
 });
